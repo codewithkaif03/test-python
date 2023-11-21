433692ae9f268723f8c1ffb0a7380455b79cd9c1
codewithkaif03 <codewithkaif.03@gmail.com>
2023-11-21 17:51:06 +0530

    file added

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Post(BaseModel):
+    name: str
+    age: int
+    publish: bool = True
+    school: Optional[str] = None
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello Kaif, how are you"}
+
+@app.get("/post")
+def get_post():
+    return {"data": "Your post is updated"}
+
+@app.post("/")
+def post_data(insta: Post = Body(...)):
+    print(insta.name)
+    return {"new_post": f"{insta.name} {insta.age}"}
+
+@app.post("/base")
+def data(new_post: Post):
+    # print(new_post)
+    print(new_post.dict())
+    return {"data": "new post"}
